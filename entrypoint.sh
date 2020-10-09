2c7760102a52ca469d44ee0a64db94347d573520
James Legg <james.legg@conversocial.com>
2020-10-09 15:45:00 +0100

    fix: path to dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+# 1st input enables dry run mode
+if [[ -n $1 ]];then
+    $SR_ARGS="--dry-run"
+fi
+
 cd $GITHUB_WORKSPACE
 
-npx semantic-release
+npx semantic-release $SR_ARGS
